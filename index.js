4a6a67a47a222157b65d63cc892df1d644f8c7d3
Gabe Abrams <gabeabrams@gmail.com>
2019-10-21 13:22:40 -0400

    Fixed proxy file usage

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import rewiremock from 'rewiremock';
+const rewiremock = require('rewiremock');
 
 // Configuration:
 rewiremock.overrideEntryPoint(module);
 
 export default (filename, substitutions) => {
-  const result = rewiremock.proxy(filename, substitutions);
+  const result = rewiremock.default.proxy(filename, substitutions);
   return result.default;
 };
